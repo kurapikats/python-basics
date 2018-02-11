8e6bf12269ca8f45680626cb2bfd8239e8c74ccc
kurapikats <kurapikats@yahoo.com>
2018-02-12 05:54:39 +0800

    add string functions

---
+++ string-functions.py
@@ -0,0 +1,9 @@
+print(":".join(['Apple', 'Banana', 'Lemon']))
+
+print("Hello There".replace("There", "Onin"))
+
+newString = "This is a string"
+print(newString.startswith("This"))
+print(newString.endswith("string"))
+print(newString.upper())
+print(newString.lower())
